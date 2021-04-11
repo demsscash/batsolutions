4ff27ae3bd24349b41127f9e07632ab907c75917
demsscash <demsscash@gmail.com>
2021-04-11 02:56:53 +0200

    modification du design

--- resources/views/layouts/master_home.blade.php
+++ resources/views/layouts/master_home.blade.php
@@ -25,9 +25,12 @@
   <link href="{{ asset('fontend/assets/vendor/owl.carousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
   <link href="{{ asset('fontend/assets/vendor/aos/aos.css') }}" rel="stylesheet">
   <link href="{{ asset('fontend/assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
-
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Glide.js/3.2.0/css/glide.core.min.css" integrity="sha512-YQlbvfX5C6Ym6fTUSZ9GZpyB3F92hmQAZTO5YjciedwAaGRI9ccNs4iw2QTCJiSPheUQZomZKHQtuwbHkA9lgw==" crossorigin="anonymous" />
+ 
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
   <!-- Template Main CSS File -->
   <link href="{{ asset('fontend/assets/css/style.css') }}" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.1/css/all.min.css">
 
   <!-- =======================================================
   * Template Name: Company - v2.1.0
@@ -68,9 +71,23 @@
   <script src="{{ asset('fontend/assets/js/main.js') }}"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r123/three.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.net.min.js"></script>
+<script src="https://tympanus.net/Development/DistortionHoverEffect/js/hover.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js" integrity="sha512-DkPsH9LzNzZaZjCszwKrooKwgjArJDiEjA5tTgr3YX4E6TYv93ICS8T41yFHJnnSmGpnf0Mvb5NhScYbwvhn2w==" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/110/three.min.js" integrity="sha512-INP2xW+d8204qbfnv0whp5pJUVgqZu8Gz8W1k0cjEZfR7/bX/8xaOeXPgxzJw6ppaxFnUN3UzIcpozU/MtGdnA==" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Glide.js/3.2.0/glide.min.js" integrity="sha512-IkLiryZhI6G4pnA3bBZzYCT9Ewk87U4DGEOz+TnRD3MrKqaUitt+ssHgn2X/sxoM7FxCP/ROUp6wcxjH/GcI5Q==" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/@glidejs/glide@3.4.1/dist/glide.js"></script>
 <script> 
    VANTA.NET('#vantajs')
 </script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/react/17.0.1/umd/react.production.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/react-dom/17.0.1/umd/react-dom.production.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/classnames/2.2.6/index.min.js"></script>
+
+<script type="text/javascript" src="https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.3.3/gsap.min.js"></script>
+<script src='https://kashyapvadi132.000webhostapp.com/uoload/1slide/anime.min.js'></script>
+		<script src='https://kashyapvadi132.000webhostapp.com/uoload/1slide/pieces.min.js'></script>
+		<script src='https://kashyapvadi132.000webhostapp.com/uoload/1slide/condi.js'></script>
 
 </body>
 
--- resources/views/pages/portfolio.blade.php
+++ resources/views/pages/portfolio.blade.php
@@ -16,41 +16,14 @@
         </div>
 
       </div>
+      
     </section><!-- End Breadcrumbs -->
-
+  <br>
+  
     <!-- ======= Portfolio Section ======= -->
-    <section id="portfolio" class="portfolio">
-      <div class="container">
+    
 
-        <div class="row" data-aos="fade-up">
-          <div class="col-lg-12 d-flex justify-content-center">
-            
-          </div>
-        </div>
-
-        <div class="row portfolio-container" data-aos="fade-up">
-
-        @foreach($images as $img)
-          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-            <img src="{{ $img->image }}" class="img-fluid" alt="">
-            <div class="portfolio-info">
-              <h4>App 1</h4>
-              <p>App</p>
-              <a href="{{ $img->image }}" data-gall="portfolioGallery" class="venobox preview-link" title="App 1"><i class="bx bx-plus"></i></a>
-               
-            </div>
-          </div>
-          @endforeach
-
-         
-
-        </div>
-
-      </div>
-    </section><!-- End Portfolio Section -->
-
-
-    <div id="vantajs"></div>
+   
 
 
 @endsection
